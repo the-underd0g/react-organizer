4d3e25da7f6d06fbdda95f9cca1b93d9eadbf484
underd0g <underd0g.3003@gmail.com>
2019-05-01 15:10:25 -0300

    changin extension

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,6 +2,9 @@ import { store } from "./store";
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Dashboard } from "./components/Dashboard";
+import { Main } from "./components/Main";
 
-ReactDOM.render()
+ReactDOM.render(
+    <Main/>,
+    document.getElementById("app")
+)
