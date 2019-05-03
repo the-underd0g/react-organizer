dd545ed91139ab5984e7cae3c6273356e7d1a545
underd0g <underd0g.3003@gmail.com>
2019-05-03 11:17:06 -0300

    adding new actions, mutations and reducers

--- src/app/components/TaskDetail.jsx
+++ src/app/components/TaskDetail.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {TaskList} from "./TaskList";
+import * as mutations from '../store/mutations';
 
 const TaskDetail = ({
     id,
     comments,
     task,
-    iscomplete
+    isComplete, setTaskCompletion,setTaskGroup,setTaskName
 })=> (
     <div>
         <div>
-            <input value={task.name}/>
+            <input onChange={setTaskName} value={task.name}/>
         </div>
         <div>
-            <button>Complete / reopen task</button>
+            <button onClick={() => setTaskCompletion(id, !isComplete)}>{isComplete ? `Reopen` : `Complete`}</button>
         </div>
         <div>
-            <select>
+            <select onChange={{setTaskGroup}} value={task.group}>
                 {groups.map(group => (
                     <option key={group.id} value={group.id}>{group.name}</option>
                 ))}
@@ -41,6 +42,22 @@ const mapStateToProps = (state, ownProps) => {
         groups,
         isComplete: task.isComplete
     }
-}
+};
 
-export const ConnectTaskDetail = connect(mapStateToProps)(TaskList);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const id = ownProps.match.params.id;
+
+    return {
+        setTaskCompletion(id,isComplete){
+            dispatch(mutations.setTaskCompletion(id, isComplete));
+        },
+        setTaskGroup(e) {
+            dispatch(mutations.setTaskGroup(id, e.target.value))
+        },
+        setTaskName(e) {
+            dispatch(mutations.setTaskName(id, e.target.value))
+        }
+    }
+};
+
+export const ConnectTaskDetail = connect(mapStateToProps,mapDispatchToProps)(TaskList);
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -14,10 +14,29 @@ export const store = createStore(
                 case mutations.CREATE_TASK:
                     return [...tasks, {
                         id: action.taskId,
+                        name: "New Task",
                         group: action.groupId,
                         owner: action.ownerId,
                         isComplete: false
-                    }]
+                    }];
+                case mutations.SET_TASK_COMPLETE:
+                    return tasks.map(task =>{
+                        return (task.id === action.taskId) ? {
+                            ...task,
+                            isComplete:action.isComplete} : task;
+                    });
+                case mutations.SET_TASK_GROUP:
+                    return tasks.map(task =>{
+                        return (task.id === action.taskId) ? {
+                            ...task,
+                            group:action.group} : task;
+                    });
+                case mutations.SET_TASK_NAME:
+                    return tasks.map(task =>{
+                        return (task.id === action.taskId) ? {
+                            ...task,
+                            name:action.name} : task;
+                    });
             }
             return tasks;
         },
