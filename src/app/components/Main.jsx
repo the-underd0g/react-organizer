14733a95bf8b13f176be8cf0b6773c45467a3f08
underd0g <underd0g.3003@gmail.com>
2019-05-05 11:35:56 -0300

    adding redirect when user is not authenticated

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,24 +2,38 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import {ConnectedDashboard} from "./Dashboard";
+import {ConnectedLogin} from "./ConnectedLogin";
 import { Router, Route } from 'react-router-dom';
 import { history } from '../store/history';
 import { ConnectedNavigation } from './Navigation';
 import { ConnectTaskDetail } from './TaskDetail';
+import { Redirect } from 'react-router';
+
+const RouteGuard = Component => ({match}) => {
+    console.info("Route Guard", match);
+    if(!store.getState().session.authenticated){
+        //reroute
+        return <Redirect to="/"/>
+    }else{
+        return <Component match={match}/>
+    }
+};
+
 
 export const Main = () => (
     <Router history={history}>
         <Provider store={store}>
             <div>
                 <ConnectedNavigation />
+                <Route exact path="/" component={ConnectedLogin} />
                 {/*<ConnectedDashboard />*/}
                 <Route exact
                        path="/dashboard"
-                       render={ () => (<ConnectedDashboard/>)}
+                       render={RouteGuard(ConnectedDashboard)}
                 />
                 <Route exact
                        path="/task/:id"
-                       render={ ({match}) => (<ConnectTaskDetail match={match}/>)}
+                       render={RouteGuard(ConnectTaskDetail)}
                 />
             </div>
         </Provider>
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -10,6 +10,9 @@ import * as mutations from './mutations';
 
 export const store = createStore(
     combineReducers({
+        session(session = defaultState.session){
+
+        },
         tasks(tasks = defaultState.tasks, action){
             switch((action.type)) {
                 case mutations.CREATE_TASK:
